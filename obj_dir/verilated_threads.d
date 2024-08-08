e0c035987bd1a84b946b56d4dab169262e6016dd
hamnamohi <hamnamohi2003@gmail.com>
2024-08-08 12:28:33 +0500

    .gitignore added

--- obj_dir/verilated_threads.d
+++
@@ -1,7 +0,0 @@
-verilated_threads.o: /home/hamna/verilator/include/verilated_threads.cpp \
- /home/hamna/verilator/include/verilatedos.h \
- /home/hamna/verilator/include/verilated_threads.h \
- /home/hamna/verilator/include/verilated.h \
- /home/hamna/verilator/include/verilated_config.h \
- /home/hamna/verilator/include/verilated_types.h \
- /home/hamna/verilator/include/verilated_funcs.h
